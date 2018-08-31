c0c63aa5793cb0274e70a33b13aaf09ff74c749b
ChrisWoolson <league@WTS14.attlocal.net>
2018-08-30 18:28:12 -0700

    Finished intro to arraylists, and finished integer stack. working on intro to stack

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -3,6 +3,7 @@ package _01_IntroToArrayLists;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,17 +16,24 @@ public class _06_IPodShuffle implements ActionListener {
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
 	ArrayList<Song> songs = new ArrayList<Song>();
+	int r;
+	Song now;
 
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
 		frame.add(panel);
+		button.addActionListener(this);
 		panel.add(button);
 		frame.setVisible(true);
 		button.setText("Suprise Me!");
 		frame.pack();
 		Song song1 = new Song("demo.mp3");
+		Song song2 = new Song("rock.mp3");
+		Song song3 = new Song("dj.mp3");
 		;
 		songs.add(song1);
+		songs.add(song2);
+		songs.add(song3);
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -46,6 +54,16 @@ public class _06_IPodShuffle implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		JButton buttonPressed = (JButton) e.getSource();
 		if (buttonPressed == button) {
+
+			if (now != null) {
+				now.stop();
+			}
+			// System.out.println("pressed");
+			Random rand = new Random();
+			r = rand.nextInt(songs.size());
+
+			now = songs.get(r);
+			now.play();
 
 		}
 
